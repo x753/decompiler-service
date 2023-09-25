693e268d540090db0ea5af80bb25d43f6bc4bb6a
Mythic <mythicmaniac@gmail.com>
2023-09-25 19:48:42 +0300

    MVP Implementation

--- app/decapi/app.py
+++ app/decapi/app.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+import asyncio
+import tempfile
+
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "world"}
+@app.post("/decompile/")
+async def decompile(file: UploadFile):
+    with tempfile.NamedTemporaryFile() as local_copy:
+        local_copy.write(await file.read())
+        local_copy.seek(0)
+
+        output = await asyncio.create_subprocess_exec(
+            "ilspycmd",
+            f"{local_copy.name}",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+
+        stdout, stderr = await output.communicate()
+
+    return {
+        "stdout": stdout,
+        "stderr": stderr,
+    }
